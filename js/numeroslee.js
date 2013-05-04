786ea0b94e82acef0f5df8e9bc0f517030afa024
Paulo Cesar Alvarez <gaelectronica@gmail.com>
2013-05-04 16:17:45 -0500

    modificación 1.0
    Primera version subida al repositorio de github

---
+++ js/numeroslee.js
@@ -0,0 +1,8 @@
+function iniciar()
+{
+	cajadatos = document.getElementById('cajadatos');
+	var boton = document.getEventListener('boton');
+	boton.addEventListener('')
+}
+
+window.addEventListener('load', iniciar, false);
